5225ebbf651064d5b9800cb7ccc321abcc9f4a22
Lanaras <lampros.lanaras@gmail.com>
2026-04-26 23:33:58 +0200

    add comments to main.sh

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,38 @@
+# To run a different experiment:
+# - Change the training script
+# - Change --variant and hyperparameters
+# - Submit again with: sbatch jobscript_slurm.sh
+
 wandb login
 
-python3 train.py \
+# -----------------------------
+# Peak Performance training
+# Variants: baseline, augsegformer, uperformer, auxlovasz_uperformer
+# -----------------------------
+
+python3 train_peak.py \
+    --variant auxlovasz_uperformer \
     --data-dir ./data/cityscapes \
-    --batch-size 64 \
+    --batch-size 2 \
     --epochs 100 \
-    --lr 0.001 \
+    --lr 0.00006 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet-training" \
+    --experiment-id final-peak-run
+
+# -----------------------------
+# Efficiency training example
+# Variants: fastscnn, c_fastscnn, kd_c_fastscnn
+# Uncomment this block and comment out the peak block above to use it.
+# -----------------------------
+
+# python3 train_efficiency.py \
+#     --variant kd_c_fastscnn \
+#     --data-dir ./data/cityscapes \
+#     --batch-size 8 \
+#     --epochs 100 \
+#     --lr 0.001 \
+#     --num-workers 10 \
+#     --seed 42 \
+#     --teacher-weights ./weights/segformer_teacher.pt \
+#     --experiment-id final-efficiency-run
